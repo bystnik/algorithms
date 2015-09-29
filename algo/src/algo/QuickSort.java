aac462c498fc032b534df097ac9a3d058850fde0
nbystrov <nbystrov@NBYSTROV.dbdc.luxoft.com>
2015-09-29 17:28:21 +0400

    Some changes

--- algo/src/algo/QuickSort.java
+++ algo/src/algo/QuickSort.java
@@ -3,8 +3,11 @@ package algo;
 public class QuickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = {100,65,3,21,55,69,231,43,21,10,1};
 
 	}
-
+	
+	
+	
+ 
 }
